1ad09b2364143b5347866b9c019eccf887998bfe
sGooze <sgooze@yandex.ru>
2016-11-18 17:28:54 +0300

    Console placeholder and launch args support added. Video and scene loading is temporary disabled.

--- src/inc/main.h
+++ src/inc/main.h
@@ -1,9 +1,6 @@
 #ifndef __MAIN_H
 #include "brd_shared.h"
-#include "brd_shader.h"
-#include "brd_texture.h"
-#include "brd_mesh.h"
-#include "brd_camera.h"
+#include "brd_video.h"
 
 #include "brd_object.h"
 #include "brd_manager.h"
@@ -11,21 +8,11 @@
 #include "brd_menu.h"
 #include "brd_console.h"
 
+#include "brd_init.h"
+
 #define __MAIN_H
 
-//Starts up SDL, creates window, and initializes OpenGL
-bool BNG_Init();
-
-//Frees media and shuts down SDL
-void close();
-
 BRD_Mesh LoadTestObject();
-
-//The window we'll be rendering to
-SDL_Window* glWindow;
-
-//OpenGL context
-SDL_GLContext glContext;
 
 #endif // __MAIN_H
 
